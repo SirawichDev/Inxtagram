2464f97998358eb833dcd613cb67f4afb75818a3
SirawichDev <sirawit0676@gmail.com>
2022-10-09 04:40:08 +0700

    Inx: update project route path

--- lib/config/custom_router.dart
+++ lib/config/custom_router.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_medium_scale_project/screens/nav/nav_screen.dart';
+import 'package:bloc_medium_scale_project/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class CustomRouter {
@@ -7,20 +9,30 @@ class CustomRouter {
       case '/':
         return MaterialPageRoute(
           settings: const RouteSettings(name: '/'),
-          builder: (_) => const Scaffold(),);
+          builder: (_) => const Scaffold(),
+        );
+      case SplashScreen.routeName:
+        return SplashScreen.route();
+      case LoginScreen.routeName:
+        return LoginScreen.route();
+      case NavScreen.routeName:
+        return NavScreen.route();
       default:
         return _errorRoute();
     }
   }
+
   static Route _errorRoute() {
-    return MaterialPageRoute( settings: const RouteSettings(name: '/error'),builder: (_) => Scaffold(
-      appBar: AppBar(
-        title:  const Text('Error')
-      ),
-      body: const Center(child: Text('something went wrong..',style: TextStyle(
-        fontSize: 20.0,
-        color: Colors.red
-      ),),),
-    ));
+    return MaterialPageRoute(
+        settings: const RouteSettings(name: '/error'),
+        builder: (_) => Scaffold(
+              appBar: AppBar(title: const Text('Error')),
+              body: const Center(
+                child: Text(
+                  'something went wrong..',
+                  style: TextStyle(fontSize: 20.0, color: Colors.red),
+                ),
+              ),
+            ));
   }
 }
