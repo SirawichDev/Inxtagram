40874d1f6a7c070225197c0d3081f877154f24f6
SirawichDev <sirawit0676@gmail.com>
2022-10-09 04:42:32 +0700

    Inx: bloc provide to ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:bloc_medium_scale_project/blocs/auth/auth_bloc.dart';
 import 'package:bloc_medium_scale_project/config/custom_router.dart';
+import 'package:bloc_medium_scale_project/repositories/repositories.dart';
+import 'package:bloc_medium_scale_project/screens/screens.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,24 +17,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Inxtagram',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(brightness: Brightness.dark,
-        color: Colors.white,
-        titleTextStyle: TextStyle(
-          color: Colors.black,
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<AuthRepository>(create: (_) => AuthRepository())
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+              create: (context) =>
+                  AuthBloc(authRepository: context.read<AuthRepository>()))
+        ],
+        child: MaterialApp(
+          title: 'Inxtagram',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              primarySwatch: Colors.blue,
+              scaffoldBackgroundColor: Colors.white,
+              appBarTheme: const AppBarTheme(
+                brightness: Brightness.dark,
+                color: Colors.white,
+                titleTextStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+                iconTheme: IconThemeData(color: Colors.black),
+              ),
+              visualDensity: VisualDensity.adaptivePlatformDensity),
+          onGenerateRoute: CustomRouter.onGenerateRoute,
+          initialRoute: SplashScreen.routeName,
         ),
-        iconTheme: IconThemeData(color: Colors.black),),
-        visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      onGenerateRoute: CustomRouter.onGenerateRoute,
-      initialRoute: '/2',
     );
   }
 }
